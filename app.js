9e73cf21c3ee9b63e47dc94515e97c667c8a70bd
Sasha <test@gmail.com>
2020-12-21 17:56:55 +0700

    added in object new data

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-console.log(numberOfFilms);
+
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,5 +9,25 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
-}
+};
+
+
+const a = prompt('Один из последние просмотренных фильмов', ''),
+      b = prompt('На сколько оцените его?', ''),  
+      c = prompt('Один из последние просмотренных фильмов', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+personalMovieDB.new = '';
+
+let test = 5;
+
+personalMovieDB.new = `Добавил строку и ${test}`;
+
+console.log(personalMovieDB);
+
+
+
 
