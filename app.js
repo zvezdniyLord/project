cecfc818754b65e86c61393d6e9bcafe546a4afd
Sasha <test@gmail.com>
2020-12-21 17:03:36 +0700

    create object personalMovieDB

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
